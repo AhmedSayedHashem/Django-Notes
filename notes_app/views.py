c4bf923c608513f14de96e8c71ada9593d25d254
ahmed-hashem <dev.ahmedsayed@gmail.com>
2020-05-16 23:07:17 +0200

    model, url, view example

--- notes_app/views.py
+++ notes_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Note
 # Create your views here.
+
+# show all notes
+def all_notes(request):
+    pass
+    # return HttpResponse('<h1> this  section retrive all notes </h1>')
+    all_notes = Note.objects.all()
+    context={
+       'allNotes' : all_notes 
+    }
+    return render(request, 'all_notes.html', context)
+# show note details
+def details(request):
+    pass
