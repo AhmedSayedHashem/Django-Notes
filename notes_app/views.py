6efdc658f18af782179b282effbb26cb9c30f6b1
ahmed-hashem <dev.ahmedsayed@gmail.com>
2020-05-18 00:13:39 +0200

    forms  example without design

--- notes_app/views.py
+++ notes_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Note
+from .forms import NoteForm
+# to import module of user 
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 # show all notes
@@ -19,3 +23,39 @@ def details(request, slug):
         'note':note
     }
     return render(request, 'noteDetails.html', context)
+
+def note_add(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+
+        if form.is_valid():
+            new_form = form.save(commit=False)
+            new_form.user = request.user
+            new_form.save()
+            return redirect('/notes')
+    else:
+        form = NoteForm()
+
+    context={
+        'form' : form,
+    }
+    return render(request, 'add.html', context)
+
+def edit(request, slug):
+    note = get_object_or_404(Note, slug=slug)
+    # print(note)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+
+        if form.is_valid():
+            new_form = form.save(commit=False)
+            new_form.user = request.user
+            new_form.save()
+            return redirect('/notes')
+    else:
+        form = NoteForm(instance=note)
+
+    context={
+        'form' : form,
+    }
+    return render(request, 'add.html', context)
